8dec2ff6b5b8c8d180b1bc2af800aa8987cbd991
Vydeeskumar <rvydeeskumar@gmail.com>
2025-06-29 10:58:59 +0530

    Debug settings

--- spotify_paatu/settings.py
+++ spotify_paatu/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&7rqj-v5sv87!)*4-l9u74@rotd3miv!3s-8!-z!79andr3p&&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 DEVELOPMENT = DEBUG
 
 
